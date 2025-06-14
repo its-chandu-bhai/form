50a6d06c9fc8b5df55284ea9bb6954bf546b3306
its-chandu-bhai <chandan844894@gmail.com>
2025-06-14 12:15:38 +0530

    Normalize line endings

---
+++ src/expenseData.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: crypto.randomUUID(),
+        title: "Mild",
+        category: 'Groceries',
+        amount: 50,
+    },
+    {
+        id: crypto.randomUUID(),
+        title: "Shirt",
+        category: 'Clothes',
+        amount: 600,
+    },
+    {
+        id: crypto.randomUUID(),
+        title: "Vegetables",
+        category: 'Groceries',
+        amount: 100,
+    },
+    {
+        id: crypto.randomUUID(),
+        title: "Electricity Bill",
+        category: 'Utilities',
+        amount: 50,
+    },
+]
