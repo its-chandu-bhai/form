d14f88fc07ded6124f987488209cac63ca508039
its-chandu-bhai <chandan844894@gmail.com>
2025-06-15 10:36:24 +0530

    In app.css file create a error class and in expenseForm.jsx file make a validate form in which create validate, handleChange for the form validataion

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ExpenseForm({ setExpenses }) {
     // const [title, setTitle] = useState('')
@@ -12,10 +12,38 @@ function ExpenseForm({ setExpenses }) {
         }
     )
 
+    const [errors, setErrors] = useState({})
+
+    const validate = (formData) => {
+        // console.log(formData)
+        const errorsData = {}
+
+        if (!formData.title) {
+            errorsData.title = 'Title is required'
+        }
+
+        if (!formData.category) {
+            errorsData.category = "Category is required"
+        }
+
+        if (!formData.amount) {
+            errorsData.amount = "Amount is required"
+        }
+
+        setErrors(errorsData)
+        return errorsData
+
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(expense)
+        // console.log(expense)
+
+        const validateResult = validate(expense)
+        console.log(validateResult)
+        // console.log(!Object.keys(validateResult).length)
+        if (Object.keys(validateResult).length) return  // If there are errors, do not proceed
+
         setExpenses((prevState) => [
             ...prevState,
             { ...expense, id: crypto.randomUUID() },
@@ -27,17 +55,28 @@ function ExpenseForm({ setExpenses }) {
         })
     }
 
+    // useEffect(() => {
+    //     console.log(expense) 
+    // }, [expense])
+
+    const handleChange = (e) => {
+        // console.log(e.target)
+        const { name, value } = e.target
+        setExpense({ ...expense, [name]: value })   // Dynamically update the state based on input name, [name] isko as a js array key treat karta hai
+        setErrors({ ...errors, [name]: '' }) // Clear the error for the specific field being edited
+    }
 
     return (
         <>
             <form className="expense-form" onSubmit={handleSubmit}>
                 <div className="input-container">
                     <label htmlFor="title">Title</label>
-                    <input id="title" name='title' value={expense.title} onChange={(e) => setExpense({ ...expense, title: e.target.value })} />
+                    <input id="title" name='title' value={expense.title} onChange={handleChange} />
+                    <p className='error'>{errors.title}</p>
                 </div>
                 <div className="input-container">
                     <label htmlFor="category">Category</label>
-                    <select id="category" name='category' value={expense.category} onChange={(e) => setExpense({ ...expense, category: e.target.value })}>
+                    <select id="category" name='category' value={expense.category} onChange={handleChange}>
                         <option hidden>Select Category</option>
                         <option value="grocery">Grocery</option>
                         <option value="clothes">Clothes</option>
@@ -45,11 +84,13 @@ function ExpenseForm({ setExpenses }) {
                         <option value="education">Education</option>
                         <option value="medicine">Medicine</option>
                     </select>
+                    <p className='error'>{errors.category}</p>
 
                 </div>
                 <div className="input-container">
                     <label htmlFor="amount">Amount</label>
-                    <input id="amount" name='amount' value={expense.amount} onChange={(e) => setExpense({ ...expense, amount: e.target.value })} />
+                    <input id="amount" name='amount' value={expense.amount} onChange={handleChange} />
+                    <p className='error'>{errors.amount}</p>
                 </div>
                 <button className="add-btn" >Add</button>
             </form>
