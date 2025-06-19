f0a804b6404e9d0d4dbbb2167b01303ba973c9da
its-chandu-bhai <chandan844894@gmail.com>
2025-06-19 11:27:39 +0530

    update the name of groceries from grocery

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -122,7 +122,7 @@ export default function ExpenseForm({ setExpenses, expense, setExpense, editingR
                 name="category"
                 value={expense.category}
                 onChange={handleChange}
-                options={['Grocery', 'Clothes', 'Bills', 'Education', 'Medicine']}
+                options={['Groceries', 'Clothes', 'Bills', 'Education', 'Medicine']}
                 defaultOption="Select Category"
                 error={errors.category}
             />
